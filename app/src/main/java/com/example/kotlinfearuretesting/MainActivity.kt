b08a199d7b4d07aae1c2359792c9bacbf4418797
Yashshree Chavan <yashshree.chavan@eLearning.com>
2019-04-01 11:23:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinfearuretesting/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinfearuretesting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      //  println(getString(R.string.app_name))
+        Log.e("app name",getString(R.string.app_name))
+    }
+}
